4f4284fed6092aa9135483cc0ad0b0e20aa79e35
Collin Meyer <collinmmeyer@gmail.com>
2022-06-07 20:11:29 -0500

    testing actions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='fdplib',
     url='https://github.com/illini-motorsports/fdplib',
-    version = 0.3,
+    version = 0.4,
     author='cmmeyer1800',
     author_email='collinmmeyer@gmail.com',
     python_requires = ">=3.6",
