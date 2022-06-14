f56a943b8cfcb9cefd95824adc7ed527b15ce3f5
Collin Meyer <collinmmeyer@gmail.com>
2022-06-14 11:13:57 -0500

    added get_var tests, and testing dev branch setup

--- tests/test_DarabData_class.py
+++ tests/test_DarabData_class.py
@@ -3,3 +3,12 @@ import fdplib.darab as darab
 def test_DarabData_loading():
     d = darab.DarabData("test_data/simple_test_data.txt")
     assert d.get_var("xtime") != None
+
+def test_DarabData_get_var():
+    d = darab.DarabData("test_data/simple_test_data.txt")
+
+    xtime = d.get_var("xtime")
+
+    assert len(xtime) == 94
+    assert type(xtime) == list
+    assert xtime[0] == 0
