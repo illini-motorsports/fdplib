7e1c7cd553e913d529223e3d9f93ddda657a0de5
Collin Meyer <collinmmeyer@gmail.com>
2022-06-14 11:09:22 -0500

    updating tests

--- tests/test_DarabData_class.py
+++ tests/test_DarabData_class.py
@@ -1,5 +1,5 @@
 import fdplib.darab as darab
 
 def test_DarabData_loading():
-    d = darab.DarabData("test_data/example.txt")
+    d = darab.DarabData("test_data/simple_test_data.txt")
     assert d.get_var("xtime") != None
