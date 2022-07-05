e200467addeb0338426fb9a82370b322e379c768
Collin Meyer <collinmmeyer@gmail.com>
2022-07-05 01:09:21 -0500

    fixing coverage pipeline

--- update_svg.sh
+++ update_svg.sh
@@ -2,9 +2,9 @@ git diff --quiet coverage.svg
 if [ $? -eq 1 ] 
 then
     echo difference in badge... adding
-    # git add coverage.svg
-    # git commit -m"updating coverage badge"
-    # git push
+    git add coverage.svg
+    git commit -m"updating coverage badge"
+    git push
 else
     echo no difference in badge... ignoring
 fi
