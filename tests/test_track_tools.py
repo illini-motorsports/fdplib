6433f2bcf60139e2e455cd6df0af7a889e868050
Collin Meyer <collinmmeyer@gmail.com>
2022-07-05 14:47:09 -0500

    changed the flow of Track class usage and greatly improved testing

--- tests/test_track_tools.py
+++ tests/test_track_tools.py
@@ -1,7 +1,8 @@
+import re
 import pytest
-
+import pickle
+import numpy as np
 from fdplib.track_tools import Track
-from fdplib.errors import VariableNotPresent
 
 
 def test_Track_initialization(track_data_path):
@@ -9,47 +10,87 @@ def test_Track_initialization(track_data_path):
 
     assert t._data.get_var("xtime") != None
 
-
-def test_Track_from_data(track_data_path):
-    t = Track()
-    t.from_data(track_data_path)
-
-    assert t._data.get_var("xtime") != None
-
-
-def test_plot_track_full(track_data_path):
+def test_Track_plot_heatmap_no_args(track_data_path):
     t = Track(track_data_path)
 
-    t.plot_track()
+    t.plot_track_heatmap()
 
-
-def test_plot_track_bounded(track_data_path):
+def test_Track_plot_heatmap_with_bounds(track_data_path):
     t = Track(track_data_path)
 
-    t.plot_track(t_bound=(0, 100))
+    t.plot_track_heatmap(t_bound=(0,100))
 
-
-def test_plot_track_missing_lat(track_data_path):
+def test_Track_plot_heatmap_with_direction(track_data_path):
     t = Track(track_data_path)
 
-    t._data.labels.remove("GPS_Long")
+    t.plot_track_heatmap(direct_arrow=True)
 
-    with pytest.raises(VariableNotPresent) as e_info:
-        t.plot_track()
-
-
-def test_plot_track_missing_long(track_data_path):
+def test_Track_plot_heatmap_with_heat_source(track_data_path):
     t = Track(track_data_path)
+    speed = t._data.get_var("speed")
 
-    t._data.labels.remove("GPS_Lat")
+    t.plot_track_heatmap(heat_source=speed)
 
-    with pytest.raises(VariableNotPresent) as e_info:
-        t.plot_track()
-
-def test_plot_track_missing_long(track_data_path):
+def test_Track_coords_from_gps(track_data_path):
     t = Track(track_data_path)
+    gps = t.coords_from_gps()
 
-    t._data.labels.remove("xtime")
+    with open("test_data/gps_coords.pkl", "rb") as FILE:
+        good_gps: np.array = pickle.load(FILE)
+    
+    np.testing.assert_array_equal(good_gps, gps)
 
-    with pytest.raises(VariableNotPresent) as e_info:
-        t.plot_track(t_bound=(0,100))
+def test_Track_coords_from_acc(track_data_path):
+    t = Track(track_data_path)
+    acc = t.coords_from_acc()
+
+    with open("test_data/acc_coords.pkl", "rb") as FILE:
+        good_acc: np.array = pickle.load(FILE)
+    
+    np.testing.assert_array_equal(good_acc, acc)
+
+def test_Track_coords_from_acc_w_yaw(track_data_path):
+    t = Track(track_data_path)
+    acc, yaw = t.coords_from_acc(ret_yaw=True)
+
+    with open("test_data/acc_coords_w_yaw.pkl", "rb") as FILE:
+        data: tuple = pickle.load(FILE)
+
+    good_acc = data[0]
+    good_yaw = data[1]
+    
+    np.testing.assert_array_equal(good_acc, acc)
+    np.testing.assert_array_equal(good_yaw, yaw)
+
+    
+
+# def test_plot_track_full(track_data_path):
+#     t = Track(track_data_path)
+
+#     t.plot_track()
+
+
+# def test_plot_track_bounded(track_data_path):
+#     t = Track(track_data_path)
+
+#     t.plot_track(t_bound=(0, 100))
+
+
+# def test_plot_track_missing_lat(track_data_path):   NEED TO GET FAKE DATA
+#     t = Track(track_data_path)
+
+#     with pytest.raises(VariableNotPresent) as e_info:
+#         t.plot_track()
+
+
+# def test_plot_track_missing_long(track_data_path):
+#     t = Track(track_data_path)
+
+#     with pytest.raises(VariableNotPresent) as e_info:
+#         t.plot_track()
+
+# def test_plot_track_missing_xtime(track_data_path):
+#     t = Track(track_data_path)
+
+#     with pytest.raises(VariableNotPresent) as e_info:
+#         t.plot_track(t_bound=(0,100))
